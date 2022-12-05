0776f59a4d2b22ef9439b0716287c575fec62a3d
Mihaela Macari <mikaela.malanici@gmail.com>
2022-12-05 23:51:03 +0200

    added started files, created project arhitecture, configured store an… …d added fonts

---
+++ app/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../slices/counterSlice";
+import { RootState } from "../store/store";
+const HomePage = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Value of counter is: {count}</h1>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+        className="w-full h-10 bg-green-400/50"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+        className="w-full h-10 bg-red-400/50"
+      >
+        Decrement
+      </button>
+    </>
+  );
+};
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["var(--roboto-font)"],
+        lato: ["var(--lato-font)"],
+        josefin: ["var(--josefin-font)"],
+      },
+    },
+  },
+  plugins: [],
+};
